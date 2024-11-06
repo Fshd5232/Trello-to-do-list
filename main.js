7b595c0524c49726bfc9a9689bd240f339c921dd
Fshd5232 <f.sh.d5232@gmail.com>
2024-11-06 10:12:24 +0330

    add style card

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './index.css'
+import './index.html'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+window.location.href = "login.html"
