c672caa41610fb10721d6f35cc5f448a45e0b4e0
Fshd5232 <f.sh.d5232@gmail.com>
2024-11-08 01:02:46 +0330

    Complete the login logic

--- src/script/login.js
+++ src/script/login.js
@@ -1,14 +1,14 @@
 let username = document.getElementById("username").value;
 let password = document.getElementById("password").value;
-let nameError = document.getElementById("nameError");
-let passwordError = document.getElementById("passError");
+let Error = document.getElementById("Error");
 const submit = document.getElementById("submit");
 const form = document.getElementById("form");
+name_user = username;
+password_user = password;
 
-username = "fatemeh";
-password = "ftm1234";
-nameError = "";
-passwordError = "";
+let name_user = "fatemeh";
+let password_user = "ftm1234";
+Error = "";
 
 submit.addEventListener("click", submit());
 form.addEventListener("submit", (e) => {
@@ -19,12 +19,24 @@ sessionStorage.setItem("username", username);
 sessionStorage.setItem("password", password);
 
 function submit() {
-  if (username != username) {
-    nameError.textContent = "Enter the correct username!"
+  if (username != name_user) {
+    Error.textContent = "Enter the correct username!";
+  }
+  if (password != password_user) {
+    Error.textContent = "Enter the correct password!";
+  }
+  if (password == password_user && username == name_user) {
+    window.location.href = "../../index.html";
   }
 }
+submit();
 
-// function switchPage(event) {
-//   event.preventDefault();
-//   window.location.href = "login.html";
-// }
+function setInput() {
+  let name = sessionStorage.getItem("name");
+  let password = sessionStorage.getItem("password");
+
+  document.getElementById("name").value = name;
+  document.getElementById("password").value = password;
+
+}
+setInput()
--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,3 @@
 import '../style/style.css'
 
-// window.location.href = "login.html"
+window.location.href = "login.html"
